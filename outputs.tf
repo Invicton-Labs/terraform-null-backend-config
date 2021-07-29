8626131c76a909d2232d53ef375edad3f26395a8
Kyle Kotowick <kyle@kotowick.ca>
2021-07-29 18:06:46 -0400

    Support for S3 and local raw backends

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,12 @@ output "workspace" {
   value       = local.workspace
 }
 
-output "state" {
-  description = "The current (pre-apply) Terraform state of this configuration, as provided by the `terraform_remote_state` data source. If no backend is configured or the `fetch_state` variable is set to `false`, this value will be `null`."
+output "output_state" {
+  description = "The current (pre-apply) Terraform state of this configuration, as provided by the `terraform_remote_state` data source. If no backend is configured or the `fetch_output_state` variable is set to `false`, this value will be `null`."
   value       = length(data.terraform_remote_state.state) > 0 ? data.terraform_remote_state.state[0].outputs : null
+}
+
+output "raw_state" {
+  description = "The raw, current (pre-apply) Terraform state of this configuration, as stored in the terraform.tfstate file (or remote equivalent). If no backend is configured or the `fetch_raw_state` variable is set to `false`, this value will be `null`."
+  value       = local.raw_state
 }
---
+++ s3.tf
@@ -0,0 +1,45 @@
+locals {
+  is_s3 = local.backend_configured ? (local.backend.type == "s3" ? true : false) : false
+}
+
+provider "aws" {
+  alias      = "backend"
+  access_key = local.is_s3 ? local.backend.config.access_key : null
+  secret_key = local.is_s3 ? local.backend.config.secret_key : null
+  region     = local.is_s3 ? local.backend.config.region : "us-east-1"
+  profile    = local.is_s3 ? local.backend.config.profile : null
+  assume_role {
+    duration_seconds    = local.is_s3 ? local.backend.config.assume_role_duration_seconds : null
+    external_id         = local.is_s3 ? local.backend.config.external_id : null
+    policy              = local.is_s3 ? local.backend.config.assume_role_policy : null
+    policy_arns         = local.is_s3 ? local.backend.config.assume_role_policy_arns : null
+    role_arn            = local.is_s3 ? local.backend.config.role_arn : null
+    session_name        = local.is_s3 ? local.backend.config.session_name : null
+    tags                = local.is_s3 ? local.backend.config.assume_role_tags : null
+    transitive_tag_keys = local.is_s3 ? local.backend.config.assume_role_transitive_tag_keys : null
+  }
+  endpoints {
+    iam      = local.is_s3 ? local.backend.config.iam_endpoint : null
+    dynamodb = local.is_s3 ? local.backend.config.dynamodb_endpoint : null
+    sts      = local.is_s3 ? local.backend.config.sts_endpoint : null
+    s3       = local.is_s3 ? local.backend.config.endpoint : null
+  }
+  shared_credentials_file     = local.is_s3 ? local.backend.config.shared_credentials_file : null
+  token                       = local.is_s3 ? local.backend.config.token : null
+  max_retries                 = local.is_s3 ? local.backend.config.max_retries : null
+  skip_credentials_validation = local.is_s3 ? local.backend.config.skip_credentials_validation : null
+  skip_metadata_api_check     = local.is_s3 ? local.backend.config.skip_metadata_api_check : null
+  skip_region_validation      = local.is_s3 ? local.backend.config.skip_region_validation : null
+  s3_force_path_style         = local.is_s3 ? local.backend.config.force_path_style : null
+}
+
+data "aws_s3_bucket_object" "state" {
+  count    = local.is_s3 && var.fetch_raw_state ? 1 : 0
+  provider = aws.backend
+  bucket   = local.backend.config.bucket
+  key      = local.backend.config.key
+}
+
+locals {
+  s3_state = length(data.aws_s3_bucket_object.state) > 0 ? jsondecode(data.aws_s3_bucket_object.state[0].body) : null
+}
