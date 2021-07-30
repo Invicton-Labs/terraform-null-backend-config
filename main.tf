1ac8ecb0f9efbd52a513373f58efd597090a5251
Kyle Kotowick <kyle@kotowick.ca>
2021-07-30 18:10:55 -0400

    Remove state file retrieval

--- main.tf
+++ main.tf
@@ -13,37 +13,12 @@ locals {
   backend            = local.backend_found ? lookup(local.backend_contents, "backend", null) : null
   backend_configured = local.backend != null
   workspace          = fileexists(local.workspace_path) ? file(local.workspace_path) : null
-  supported_backends = [
-    "s3",
-    "local"
-  ]
 }
 
 // Fetch the state, if desired
 data "terraform_remote_state" "state" {
-  count     = local.backend_configured && var.fetch_output_state ? 1 : 0
+  count     = local.backend_configured && var.fetch_remote_state ? 1 : 0
   backend   = local.backend.type
   config    = local.backend.config
   workspace = local.workspace
-}
-
-// Ensure that the backend type is supported
-module "assert_valid_backend" {
-  source        = "Invicton-Labs/assertion/null"
-  version       = "0.1.2"
-  condition     = var.fetch_raw_state && local.backend_configured ? contains(local.supported_backends, local.backend.type) : true
-  error_message = local.backend_configured ? "The '${local.backend.type}' backend type is not supported for the 'Invicton-Labs/backend-config/null' module." : null
-}
-
-locals {
-  raw_states = [
-    local.default_state,
-    local.s3_state
-  ]
-  non_null_raw_states = [
-    for state in local.raw_states :
-    state
-    if state != null
-  ]
-  raw_state = length(local.non_null_raw_states) > 0 ? local.non_null_raw_states[0] : null
 }
--- variables.tf
+++ variables.tf
@@ -4,14 +4,8 @@ variable "working_dir" {
   default     = null
 }
 
-variable "fetch_output_state" {
+variable "fetch_remote_state" {
   description = "Whether the current (pre-apply) Terraform state of this configuration should be retrieved, as provided by the `terraform_remote_state` data source."
-  type        = bool
-  default     = false
-}
-
-variable "fetch_raw_state" {
-  description = "Whether the raw, current (pre-apply) Terraform state of this configuration should be retrieved, as stored in the terraform.tfstate file (or remote equivalent)."
   type        = bool
   default     = false
 }
